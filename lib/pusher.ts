cc38aca0069fe2d75d398524635350b24806ec55
Vishakha Agrawal <vishakhaagrawal0508@gmail.com>
2026-03-06 23:24:53 +0530

    Initial commit

---
+++ lib/pusher.ts
@@ -0,0 +1,39 @@
+import PusherServer from "pusher";
+import PusherClient from "pusher-js";
+
+// in development this will create multiple instances of pusher,
+// which might cause you to hit the connection limit in free tier
+// export const pusherServer = new PusherServer({
+//   appId: process.env.PUSHER_APP_ID!,
+//   key: process.env.PUSHER_APP_KEY!,
+//   secret: process.env.PUSHER_APP_SECRET!,
+//   cluster:"eu",
+//   useTLS: true
+// })
+
+// export const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
+//   cluster:"eu",
+// })
+
+declare global {
+	var pusherServer: PusherServer | undefined;
+	var pusherClient: PusherClient | undefined;
+}
+
+export const pusherServer =
+	(globalThis as any).pusherServer ||
+	new PusherServer({
+		appId: process.env.PUSHER_APP_ID!,
+		key: process.env.PUSHER_APP_KEY!,
+		secret: process.env.PUSHER_APP_SECRET!,
+		cluster: "eu",
+		useTLS: true,
+	});
+
+export const pusherClient =
+(globalThis as any).pusherClient || new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, 
+		{ cluster: "eu" });
+
+
+
+		
